12f73d6a5918751d2997d8ab9677ff10f3bed694
cu3e <bodzioch.mt@gmail.com>
2017-02-14 19:48:33 +0100

    Added & style badges from codeshool endpoint

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,27 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/cu3e.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log(response.courses.completed);
+
+      var badges = $('#badges'),
+          tmp = '';
+
+      $.each(response.courses.completed, function(index, val){
+
+        tmp += '<div class="course">';
+        tmp += '<h3>' + val.title + '</h3>';
+        tmp += '<img src="' + val.badge + '">';
+        tmp += '<a href="' + val.url + '" target="_blank" class="btn btn-primary">' + 'See Course' + '</a>';
+        tmp += '</div>';
+
+      });
+
+      badges.append(tmp);
+
+    }
+  });
 
 });
